6417191323fc9d3f77c07fac5a420c890a4b875a
FedeC <24813315+turbostar190@users.noreply.github.com>
2023-05-03 16:06:22 +0200

    Cleanup

--- krack-client.py
+++ krack-client.py
@@ -2,12 +2,13 @@
 
 """This code tests if a client is affected by CVE-2017-13077 and 
 CVE-2017-13078 (KRACK attack) and determine whether an 
-implementation is vulnerable to attacks."""
+implementation is vulnerable to attacks in a Mininet-wifi environment."""
 
-__author__ = "Ramon Fontes and 2022/2023 Group4"
-__credits__ = ["https://github.com/vanhoefm/krackattacks-test-ap-ft"]
+__author__ = "Ramon Fontes and UNITN 2022/2023 Network Security master course Group14 Lab"
+__credits__ = ["https://github.com/vanhoefm/krackattacks-scripts"]
 
 from time import sleep
+import subprocess
 
 from mininet.log import setLogLevel, info
 from mininet.term import makeTerm
@@ -38,20 +39,30 @@ def topology():
     info("*** Starting network\n")
     net.build()
 
+
     # Monitor mode, or RFMON (Radio Frequency MONitor) mode, allows a computer with a wireless network 
     # interface controller (WNIC) to monitor all traffic received on a wireless channel
     ap1.cmd("iw dev ap1-wlan0 interface add mon0 type monitor")
     ap1.cmd("ifconfig mon0 up")
     sta1.cmd("iw dev sta1-wlan0 interface add mon0 type monitor")
     sta1.cmd("ifconfig mon0 up")
+    
+    # print wpa_supplicant version
+    wpa_s = subprocess.Popen(["wpa_supplicant"], stdout=subprocess.PIPE)
+    output = subprocess.check_output(("grep", "v[0-9].[0-9]*"), stdin=wpa_s.stdout)
+    wpa_s.wait()
+    info("Using %s\n" % output)
 
     sleep(10)
 
     # launch script listener script on the access point
     makeTerm(ap1, title='AP', cmd="bash -c 'cd krackattacks-scripts/krackattack && source venv/bin/activate && python krack-test-client.py;'")
+
     sleep(5)
+    
+    # remove any previosly opened interfaces
     sta1.cmd("killall wpa_supplicant")
-    # we connect the client to the AP
+    # connect the client to the AP
     makeTerm(sta1, title='Connection', cmd="bash -c 'wpa_supplicant -i sta1-wlan0 -c network.conf'")
 
     info("*** Running CLI\n")
--- krack-ft.py
+++ krack-ft.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
 """This code tests if APs are affected by CVE-2017-13082 (KRACK attack) and
-determine whether an implementation is vulnerable to attacks on FT protocol 802.11r."""
+determine whether an implementation is vulnerable to attacks on FT protocol 802.11r
+in a Mininet-wifi environment."""
 
-__author__ = "Ramon Fontes and 2022/2023 Group4"
-__credits__ = ["https://github.com/vanhoefm/krackattacks-test-ap-ft"]
+__author__ = "Ramon Fontes and UNITN 2022/2023 Network Security master course Group14 Lab"
+__credits__ = ["https://github.com/vanhoefm/krackattacks-scripts"]
 
 from time import sleep
+import subprocess
+import os
 
 from mininet.log import setLogLevel, info
 from mininet.term import makeTerm
@@ -14,7 +17,6 @@ from mn_wifi.net import Mininet_wifi
 from mn_wifi.cli import CLI
 from mn_wifi.link import wmediumd
 from mn_wifi.wmediumdConnector import interference
-import os
 
 
 def topology():
@@ -24,11 +26,20 @@ def topology():
 
     info("*** Creating nodes\n")
     sta1 = net.addStation('sta1', ip='10.0.0.1/8', position='50,0,0',
-                          encrypt='wpa2')
-    ap1 = net.addStation('ap1', mac='02:00:00:00:01:00',
-                         ip='10.0.0.101/8', position='10,30,0')
-    ap2 = net.addStation('ap2', mac='02:00:00:00:02:00',
-                         ip='10.0.0.102/8', position='100,30,0')
+                          encrypt='wpa2')    
+
+    info("*** Configuring AP settings\n")
+    # Configuration of access point 1 and 2
+    ap1 = net.addAccessPoint('ap1',  mac='02:00:00:00:01:00', ip='10.0.0.101/8',
+                             ssid="handover", mode="g", 
+                             channel="1", ieee80211r='yes', mobility_domain='a1b2', 
+                             passwd='123456789a', encrypt='wpa2', position='10,30,0', 
+                             inNamespace=True, datapath="user", failMode="standalone")
+    ap2 = net.addAccessPoint('ap2', mac='02:00:00:00:02:00', ip='10.0.0.102/8', 
+                             ssid="handover", mode="g", 
+                             channel="6", ieee80211r='yes', mobility_domain='a1b2', 
+                             passwd='123456789a', encrypt='wpa2', position='100,30,0', 
+                             inNamespace=True, datapath="user", failMode="standalone")
 
     info("*** Configuring Propagation Model\n")
     net.setPropagationModel(model="logDistance", sL=0.4, exp=3.5)
@@ -39,33 +50,37 @@ def topology():
     info("*** Linking nodes\n")
     net.addLink(ap1, ap2)
 
-    info("*** Configuring AP settings\n")
-    # Configuration of access point 1 and 2
-    # Master is the mode acting as an access point
-    ap1.setMasterMode(intf='ap1-wlan0', ssid='handover', channel='1',
-                      ieee80211r=True, mobility_domain='a1b2',
-                      passwd='123456789a', encrypt='wpa2', datapath="user", failMode="standalone")
-    ap2.setMasterMode(intf='ap2-wlan0', ssid='handover', channel='6',
-                      ieee80211r=True, mobility_domain='a1b2',
-                      passwd='123456789a', encrypt='wpa2', datapath="user", failMode="standalone")
-
     info("*** Plotting Graph\n")
     net.plotGraph(min_x=-100, min_y=-100, max_x=200, max_y=200)
 
     info("*** Starting network\n")
     net.build()
 
+
     # Monitor mode, or RFMON (Radio Frequency MONitor) mode, allows a computer with a wireless network 
     # interface controller (WNIC) to monitor all traffic received on a wireless channel
     sta1.cmd("iw dev sta1-wlan0 interface add mon0 type monitor")
     sta1.cmd("ifconfig mon0 up")
 
+    # print hostapd version (hostapd by default outputs on stderr???)
+    hostapd = subprocess.Popen(["hostapd"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        output = subprocess.check_output(("grep", "v[0-9].[0-9]*"), stdin=hostapd.stderr)
+    except: 
+        output = subprocess.check_output(("grep", "v[0-9].[0-9]*"), stdin=hostapd.stdout)
+    hostapd.wait()
+    info("Using %s\n" % output)
+
     sleep(5)
+
     # We need AP scanning. Otherwise, roam won't work
     makeTerm(sta1, title='Scanning', cmd="bash -c 'echo \"AP Scanning\" && wpa_cli -i sta1-wlan0 scan; read;'")
-    # Initialize the FT test monitor
+
     sleep(15)
+
+    # remove any previosly opened interfaces
     sta1.cmd("killall wpa_supplicant")
+    # Initialize the FT test monitor
     makeTerm(sta1, title='KrackAttack', cmd="bash -c 'cd krackattacks-scripts/krackattack && source venv/bin/activate && python krack-ft-test.py wpa_supplicant -D nl80211 -i sta1-wlan0 -c ../../sta1-wlan0_0.staconf;'")
     makeTerm(sta1, title='wpa_cli')
 
